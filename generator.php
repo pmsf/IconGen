ad73d26703cad8529faac65b34e917d2f65cf4e2
wheel <jepke@users.noreply.github.com>
2018-10-25 16:19:14 +0200

    Form still kinda buggy

--- generator.php
+++ generator.php
@@ -31,31 +31,56 @@ foreach ( $pokemons as $k => $pokemon ) {
 	// Draw colored circle
 	imagefilledellipse($img, 40, 40, 80, 80, $circlecolor);
 	// Define text font
-	$font_path = '/opt/safespritesgenerator/Aller_Rg.ttf';
+	$id_font_path = '/opt/safespritesgenerator/Lato-Black.ttf';
+	$name_font_path = '/opt/safespritesgenerator/Lato-Bold.ttf';
+	$form_font_path = '/opt/safespritesgenerator/Lato-Regular.ttf';
 	$font_size = 15;
+	$form_font_size = 12;
 	$hasforms = isset( $pokemon['forms'] );
 	if ( $hasforms ) {
 		foreach ( $pokemon['forms'] as $f ) {
 			$protoform = $f['protoform'];
-			imagettftext($img, 20, 0, 18, 24, $white, $font_path, $id);
-			imagettftext($img, 15, 0, 1, 47, $white, $font_path, $pokemon['name']);
-			imagettftext($img, 15, 0, 1, 70, $white, $font_path, $f['nameform']);
+			$nameform = $f['nameform'];
+			$namebbox = imagettfbbox($font_size, 0, $name_font_path, $pokemon['name']);
+			$name_image_width = abs($namebbox[4] - $namebbox[0]);
+			if ( $name_image_width <= 80 ) {
+				$name_x = floor((80 - $name_image_width) / 2 + 2);
+			} else {
+				$font_size = 15 - 4;
+				$namebbox = imagettfbbox($font_size, 0, $name_font_path, $pokemon['name']);
+				$name_image_width = abs($namebbox[4] - $namebbox[0]);
+				$name_x = floor((80 - $name_image_width) / 2 + 2);
+			}
+
+			$formbbox = imagettfbbox($font_size, 0, $form_font_path, $pokemon['name']);
+			$form_image_width = abs($formbbox[4] - $formbbox[0]);
+			if ( $form_image_width <= 80 ) {
+				$form_x = floor((80 - $form_image_width) / 2 + 10);
+			} else {
+				$form_font_size = 15 - 4;
+				$formbbox = imagettfbbox($font_size, 0, $form_font_path, $pokemon['name']);
+				$form_image_width = abs($formbbox[4] - $formbbox[0]);
+				$form_x = floor((80 - $form_image_width) / 2 + 10);
+			}
+
+			imagettftext($img, 20, 0, 16, 26, $white, $id_font_path, $id);
+			imagettftext($img, $font_size, 0, $name_x, 48, $white, $name_font_path, $pokemon['name']);
+			imagettftext($img, $form_font_size, 0, $form_x, 65, $white, $form_font_path, $nameform);
 			imagepng($img, "icons/pokemon_icon_" . $id . "_" . $protoform . ".png");
 		}
 	} else {
-		$bbox = imagettfbbox($font_size, 0, $font_path, $pokemon['name']);
+		$bbox = imagettfbbox($font_size, 0, $name_font_path, $pokemon['name']);
 		$image_width = abs($bbox[4] - $bbox[0]);
 		if ( $image_width <= 80 ) {
-			$x = floor((80 - $image_width) / 2);
+			$x = floor((80 - $image_width) / 2 + 2);
 		} else {
-			$font_size = 15 - 2;
-			$bbox = imagettfbbox($font_size, 0, $font_path, $pokemon['name']);
+			$font_size = 15 - 4;
+			$bbox = imagettfbbox($font_size, 0, $name_font_path, $pokemon['name']);
 			$image_width = abs($bbox[4] - $bbox[0]);
-			echo $image_width;
-			$x = floor((80 - $image_width) / 2);
+			$x = floor((80 - $image_width) / 2 + 2);
 		}
-		imagettftext($img, 20, 0, 18, 24, $white, $font_path, $id);
-		imagettftext($img, $font_size, 0, $x, 47, $white, $font_path, $pokemon['name']);
+		imagettftext($img, 20, 0, 16, 26, $white, $id_font_path, $id);
+		imagettftext($img, $font_size, 0, $x, 48, $white, $name_font_path, $pokemon['name']);
 		imagepng($img, "icons/pokemon_icon_" . $id . "_00.png");
 	}
 }
