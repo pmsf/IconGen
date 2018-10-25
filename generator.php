222f1796d6ec9862e395c890e69379d2aa461c11
wheel <jepke@users.noreply.github.com>
2018-10-25 12:59:44 +0200

    Numbers only script

--- generator.php
+++ generator.php
@@ -28,8 +28,6 @@ foreach ( $pokemons as $k => $pokemon ) {
 	$white = imagecolorallocate($img, 255, 255, 255);
 	$black = imagecolorallocate($img, 0, 0, 0);
 	$circlecolor = imagecolorallocate($img, $r, $g, $b);
-	// Draw outer arc
-	imageArc($img, 40, 40, 80, 80, 0, 360, $black);
 	// Draw colored circle
 	imagefilledellipse($img, 40, 40, 80, 80, $circlecolor);
 	// Define text font
@@ -39,14 +37,14 @@ foreach ( $pokemons as $k => $pokemon ) {
 	if ( $hasforms ) {
 		foreach ( $pokemon['forms'] as $f ) {
 			$protoform = $f['protoform'];
-			imagettftext($img, 20, 0, 18, 23, $white, $font_path, $id);
-			imagettftext($img, 15, 0, 1, 46, $white, $font_path, $pokemon['name']);
+			imagettftext($img, 20, 0, 18, 24, $white, $font_path, $id);
+			imagettftext($img, 15, 0, 1, 47, $white, $font_path, $pokemon['name']);
 			imagepng($img, "icons/pokemon_icon_" . $id . "_" . $protoform . ".png");
-			imagettftext($img, 15, 0, 1, 69, $white, $font_path, $f['nameform']);
+			imagettftext($img, 15, 0, 1, 70, $white, $font_path, $f['nameform']);
 		}
 	} else {
-		imagettftext($img, 20, 0, 18, 23, $white, $font_path, $id);
-		imagettftext($img, 15, 0, 1, 46, $white, $font_path, $pokemon['name']);
+		imagettftext($img, 20, 0, 18, 24, $white, $font_path, $id);
+		imagettftext($img, 15, 0, 1, 47, $white, $font_path, $pokemon['name']);
 		imagepng($img, "icons/pokemon_icon_" . $id . "_00.png");
 	}
 
